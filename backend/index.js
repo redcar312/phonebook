e11dfca15a0c44955f75dcb5eed98aa5e38e5352
Matias Hurtamo <matias.h.hurtamo@gmail.com>
2023-08-29 01:28:51 +0300

    asd

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 let persons = [
       { 
         "name": "Arto Hellas", 
